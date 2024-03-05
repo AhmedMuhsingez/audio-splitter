c0a2c779c927d029fe9a5b2f3399d18573d6fb47
AhmedMuhsingez <76662243+AhmedMuhsingez@users.noreply.github.com>
2024-03-05 20:22:33 +0300

    added addition to fileName & new function to remove characters from name

--- fileName.js
+++ fileName.js
@@ -1,38 +1,34 @@
 import fs from "fs/promises";
 import path from "path";
 import fsNormal from "fs";
-
-// import { readLine } from "readline";
-// import * as readline from "node:readline/promises";
 import promptSync from "prompt-sync";
 const prompt = promptSync();
-
-const inFolder = "./in/names";
+const inFolder = "./in/names/";
+const typeOfProcess = prompt("Add or sub? (Type add/sub) ");
+const amount = parseInt(prompt("Enter addition or subtraction amount: "), 10); // Parse amount as an integer
 
 const processFiles = async () => {
-	const amount = prompt("Enter subtraction amount: ");
 	try {
 		const inFiles = await fs.readdir(inFolder);
 
 		for (const inFile of inFiles) {
 			const inFilePath = path.join(inFolder, inFile);
 			const inFilename = path.parse(inFile).name;
+			const parsedFilename = parseInt(inFilename, 10); // Parse inFilename as an integer
 
-			// Subtract 1 from the filename
-			const newFilename = parseInt(inFilename, 10) - amount;
+			const newFilename =
+				typeOfProcess === "sub"
+					? parsedFilename - amount // Perform subtraction
+					: parsedFilename + amount; // Perform addition
 			if (!fsNormal.existsSync("./out/newNames")) await fs.mkdir("./out/newNames");
-
 			// Create the output file path
 			const outFilePath = path.join(
 				"./out/newNames",
 				`${newFilename}${path.extname(inFile)}`
 			);
-
 			console.log(`Processing ${inFile}. Renaming to ${path.basename(outFilePath)}`);
-
 			// Rename the file
 			await fs.rename(inFilePath, outFilePath);
-
 			console.log(`Processing of ${inFile} complete.`);
 		}
 	} catch (error) {
@@ -42,3 +38,19 @@ const processFiles = async () => {
 
 // Call the function
 processFiles();
+
+//Remove specific character from the file name:
+
+// const removeTwo = path.parse(inFile).name.slice(0, -1); // Remove the last character from the file name
+// const newFileName = path.join(
+// 	// Create a new file name by appending the extension
+// 	"./out/newNames",
+// 	`${removeTwo}${path.extname(inFile)}`
+// );
+// if (!fsNormal.existsSync("./out/newNames")) await fs.mkdir("./out/newNames");
+
+// console.log(`Processing ${inFile}. Renaming to ${path.basename(newFileName)}`);
+
+// await fs.rename(inFile, newFileName); // Rename the file with the new file name
+
+// console.log(`Processing of ${inFile} complete.`);
